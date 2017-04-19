a0287c6fbf905478a05c7cdbb86f0381c7bad7ee
Jobin Lawrance <jobinlawrance@gmail.com>
2017-04-19 22:46:00 +0530

    add encrypted rsa

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="source"
 TARGET_BRANCH="master"
 
-# # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#     echo "Skipping deploy; Either a pull request or chages to non master branch"
-#     exit 0
-# fi
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; Either a pull request or chages to non source branch"
+    exit 0
+fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -31,21 +31,22 @@ npm run build
 # Copy all the build files to out folder 
 cp -r dist/* out/
 
-# Now let's go have some fun with the cloned repo
-# cd out
-# git config user.name "Travis CI"
-# git config user.email "jobinlawrance@gmail.com"
+#Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Travis CI"
+git config user.email "jobinlawrance@gmail.com"
 
-# # decrypt private ssh key using travis environment variables and add to ssh agent
-# openssl aes-256-cbc -K $encrypted_5ce7885c22c1_key -iv $encrypted_5ce7885c22c1_iv -in ../travis_vue_news_rsa.enc -out travis_vue_news_rsa -d
-# chmod 600 travis_vue_news_rsa
-# eval `ssh-agent -s`
-# ssh-add travis_vue_news_rsa
+# decrypt private ssh key using travis environment variables and add to ssh agent
+openssl aes-256-cbc -K $encrypted_5ed29cc41e6d_key -iv $encrypted_5ed29cc41e6d_iv -in ../travis_rsa.enc -out travis_rsa -d
 
-# # Commit the "changes", i.e. the new version.
-# # The delta will show diffs between new and old versions.
-# git add .
-# git commit -m "Deploy to GitHub Pages: ${SHA}"
+chmod 600 travis_rsa
+eval `ssh-agent -s`
+ssh-add travis_rsa
 
-# # Now that we're all set up, we can push.
-# git push $SSH_REPO $TARGET_BRANCH
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
