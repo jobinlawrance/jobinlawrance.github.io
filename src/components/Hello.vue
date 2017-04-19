608650022a25fda0bf1efe900045be9b064a9474
Jobin Lawrance <jobinlawrance@gmail.com>
2017-04-20 01:40:48 +0530

    Add styling to basic name and subtitle

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,36 +1,32 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <section class="hero is-fullheight ">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1">
+          Jobin Lawrance
+        </h1>
+        <h2 class="subtitle is-3">
+          {{ caption }}
+        </h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Website is coming soon..'
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        name: 'Jobin Lawrance',
+        caption: 'Web & App Developer'
+      }
     }
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped lang="sass">
+  @import 'main.scss';
 </style>
